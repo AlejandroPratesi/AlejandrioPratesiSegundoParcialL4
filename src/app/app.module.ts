95d28c61e67e00e26a736c89e51cb6b0748152f4
Alejandro <aspratesi@gmail.com>
2019-10-28 11:00:00 -0300

    1 - Componentes Registrarse, login, Home, y error 2 - Modulo de ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { PaginaNoEncontradaComponent } from './componentes/pagina-no-encontrada/pagina-no-encontrada.component';
+import { RegistrarseComponent } from './componentes/registrarse/registrarse.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PaginaNoEncontradaComponent,
+    RegistrarseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
